3fe21556fbf265203b3be6c1e61df1b7f4728110
kotvnasketer2 <martiner8915@gmail.com>
2025-06-20 16:37:55 -0400

    Add files via upload

---
+++ RustScript.cpp
@@ -0,0 +1,7 @@
+// IcJSDTBHuM
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello from function 7b15632fd662436293daf10853a26d1f_20250619080305698424!" << endl;
+    return 0;
+}
---
+++ RustScript.rs
@@ -0,0 +1,4 @@
+// IcJSDTBHuM
+fn main() {
+    println!("Hello from function 7b15632fd662436293daf10853a26d1f_20250619080305698424!");
+}
